06ea7fe77aec4c9a638eeba8559bb4dc72062989
iuka <2628969483@qq.com>
2024-11-24 19:46:07 +0800

    feat: 添加字段

--- app/models/test.model.js
+++ app/models/test.model.js
@@ -8,6 +8,42 @@ module.exports = (sequelize, Sequelize) => {
       },
       'bloodType': {
         type: Sequelize.STRING
+      },
+      'sex': {
+        type: Sequelize.STRING
+      },
+      'phone': {
+        type: Sequelize.DECIMAL
+      },
+      'height': {
+        type: Sequelize.DOUBLE
+      },
+      'weight': {
+        type: Sequelize.DOUBLE
+      },
+      'address': {
+        type: Sequelize.STRING
+      },
+      'doctorName':{
+        type: Sequelize.STRING
+      },
+      'DR1': {
+        type: Sequelize.STRING
+      },
+      'DR2': {
+        type: Sequelize.STRING
+      },
+      'DP1': {
+        type: Sequelize.STRING  
+      },
+      'DP2': {
+        type: Sequelize.STRING
+      },
+      'DQ1':{
+        type: Sequelize.STRING  
+      },
+      'DQ2': {
+        type: Sequelize.STRING
       }
     });
   
